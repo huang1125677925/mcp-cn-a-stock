e7736d78dbe3ad6bdc63a3b45a730950e0010ed7
LiJia <lijia.c@gmail.com>
2025-03-07 18:06:50 +0800

    add desc for gpt

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
- gpt -M "http://82.156.17.205/cnstock/sse" "请详细分析一下浦发银行的最近走势" 
+# `gpt` 是一个大模型命令行工具，支持多种模型和接口，参见 https://github.com/elsejj/gpt
 
- gpt -M "http://82.156.17.205/cnstock/sse" "请从技术分析角度分析一下贵州茅台的后市走势" 
+gpt -M "http://82.156.17.205/cnstock/sse" "请详细分析一下浦发银行的最近走势" 
+
+gpt -M "http://82.156.17.205/cnstock/sse" "请从技术分析角度分析一下贵州茅台的后市走势" 
